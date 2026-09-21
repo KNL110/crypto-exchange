d3ba414700e1af87e190592f7c9a4eb2d6607ed4
Krunal Asari <asarikrunal9@gmail.com>
2026-09-21 16:36:50 +0530

    feat: implement basic server setup with Echo framework and add initial route

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-//TODO: implement Doubly linked list for optimzation to keep sorted order on time, and O(1) removal 
-// of orders from limit to maintain FIFO order.
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
 
-func main(){
-	fmt.Println("working yeah!!!")
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
+	})
+
+	if err := e.Start(":3000"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/knl110/crypto-exchange
 
 go 1.27.1
+
+require github.com/labstack/echo/v5 v5.3.1
+
+require golang.org/x/time v0.15.0 // indirect
